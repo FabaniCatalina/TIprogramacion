b03999489514d2225039c5484ad50e5f7a479bcd
sophiadeleone <sdeleone@udesa.edu.ar>
2023-11-18 01:12:48 -0930

    .

--- js/genres.js
+++ js/genres.js
@@ -1,11 +1,15 @@
 let API = "0cfe01fc90041a85b25d69b07b987148";
 
+
 let purl=`https://api.themoviedb.org/3/genre/movie/list?api_key=${API}`
 let surl=`https://api.themoviedb.org/3/genre/tv/list?api_key=${API}`
 
+
 let qS = location.search
 let queryStringObject = new URLSearchParams(qS)
-let GG = queryStringObject.get('busqueda') 
+let GG = queryStringObject.get('busqueda')
+
+
 
 
 fetch(purl)
@@ -14,41 +18,57 @@ fetch(purl)
     })
     .then(function(data){
         console.log(data)
-        let listGenreP = data.results
-        let topSerie= document.querySelector('.ggP');
-        topSerie.innerHTML+=`Generos Peliculas:`
-        let lista_generos = []
+        let listGenreP = data.genres
+        console.log(listGenreP)
+        let lista_generos = document.querySelector(".grupo_gg1")
+        let gege = ""
+        
         for(let i=0; i<data.genres.length; i++){
-            lista_generos = data.genres[i].name + "  "
-            let article=document.querySelector('.gg1');
-            article.innerHTML+=`<a href="detail-genres.html?id=${data.genres[i].id}&name=${lista_generos}&type=movie"><article ><p>${lista_generos}</p></article></a>`
-
+            gege += `<article>
+                        <ul>
+                            <li>
+                                <h2>
+                                    <a href=./genre_detail.html?id=${listGenreP[i].id}listaGenreP[i].title}</a>
+                                </h2>
+                            <li>
+                        </ul>
+                    </article>`;
         }
-       
+        
+        lista_generos.innerHtML = gege;
     })
-    .catch(function(error){
+    .catch(function(error)) {
         console.log(error);
-    })
-
-    fetch(surl)
-    .then(function(response){
-        return response.json();
-    })
-    .then(function(data){
-        console.log(data)
-        let listGenreS = data.results
-        let topSerie= document.querySelector('.gS');
-        topSerie.innerHTML+=`Generos Series:`
-        let lista_generos = []
-        for(let i=0; i<data.genres.length; i++){
-            lista_generos = data.genres[i].name + "  "
-            let article=document.querySelector('.gg2');
-            article.innerHTML+=`<a href="detail-genres.html?id=${data.genres[i].id}&name=${lista_generos}&type=serie"><article ><p>${lista_generos}</p></article></a>`
-
         }
-        console.log(lista_generos)
-       
-    })
-    .catch(function(error){
-        console.log(error);
-    })
+        
+    
+    
+  fetch(surl)
+      .then(function(response){
+          return response.json();
+      })
+      .then(function(data){
+          console.log(data)
+          let listGenreS = data.genres
+          console.log(listGenreS)
+          let lista_generos = document.querySelector(".grupo_gg1")
+          let geges = ""
+          
+          for(let i=0; i<data.genres.length; i++){
+              geges += `<article>
+                          <ul>
+                              <li>
+                                  <h2>
+                                      <a href=./genre_detail.html?id=${listGenreS[i].id}listaGenreS[i].name}</a>
+                                  </h2>
+                              <li>
+                          </ul>
+                      </article>`;
+          }
+          
+          lista_generos.innerHtML = geges;
+      })
+      .catch(function(error)) {
+          console.log(error);
+          }      
+      
